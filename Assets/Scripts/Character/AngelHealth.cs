ce9b037201c628841d8c6081fae138c987078125
Deng JunYuan <540997806@qq.com>
2018-12-08 12:29:36 +0800

    UI基本逻辑完成

--- Assets/Scripts/Character/AngelHealth.cs
+++ Assets/Scripts/Character/AngelHealth.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AngelHealth : CharacterHealth {
+    public Slider WinSlider;
     public float maxHp;
     public float currentHp;
     // Use this for initialization
@@ -19,7 +21,10 @@ public class AngelHealth : CharacterHealth {
     {
         base.TakeDamage(damage, attackerID);
         currentHp -= damage;
-        if(currentHp <= 0)
+        if (WinSlider)
+            WinSlider.value = currentHp;
+        else Debug.LogError("No Slider");
+        if (currentHp <= 0)
         {
             Debug.Log("Die");
         }
