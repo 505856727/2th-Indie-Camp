e197d4d6c897ff934334f25121ff95e6d6f7898f
505856727 <30371192+505856727@users.noreply.github.com>
2018-12-09 00:52:50 +0800

    不知道

---
+++ Assets/jiaer/AudioManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+    private static AudioManager instance;
+    public AudioClip[] Music;
+    public AudioClip[] Sound;
+    public GameObject MusicManager;
+    public GameObject SoundManager;
+	// Use this for initialization
+	void Start () {
+        instance = this;
+	}
+
+    public static AudioManager GetInstance()
+    {
+        return instance;
+    }
+
+    public void PlayMusic(int ID)
+    {
+        MusicManager.GetComponent<AudioSource>().clip = Music[ID];
+        MusicManager.GetComponent<AudioSource>().Play();
+    }
+
+    public void PlaySound(int ID)
+    {
+        SoundManager.GetComponent<AudioSource>().clip = Music[ID];
+        SoundManager.GetComponent<AudioSource>().Play();
+    }
+	
+
+}
