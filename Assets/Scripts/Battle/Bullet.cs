cf46c21c28c6c60ab0c985c33b980a972cf7f63b
Deng JunYuan <540997806@qq.com>
2018-12-08 15:49:19 +0800

    修复射击间隔

--- Assets/Scripts/Battle/Bullet.cs
+++ Assets/Scripts/Battle/Bullet.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour {
     public string attackerID;
     public float damage = 10.0f;
-    public float speed = 1.0f;
+    public float speed = 100.0f;
     public float duration = 1.0f;
     public Vector3 forwardVec;
 	// Use this for initialization
